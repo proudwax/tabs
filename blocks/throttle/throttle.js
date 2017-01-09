7350e1414c86b5a28e01c34033ed74bc50b70124
Ivan <yazvyazda@mail.ru>
2017-01-09 17:56:50 +0300

    add files

---
+++ blocks/throttle/throttle.js
@@ -0,0 +1,30 @@
+modules.define('throttle', function(provide) {
+    provide(
+        function (func, ms) {
+            var isThrottled = false,
+                savedArgs,
+                savedThis;
+
+            function wrapper() {
+                if (isThrottled) {
+                    savedArgs = arguments;
+                    savedThis = this;
+                    return;
+                }
+
+                func.apply(this, arguments);
+
+                isThrottled = true;
+
+                setTimeout(function() {
+                    isThrottled = false;
+                    if (savedArgs) {
+                        wrapper.apply(savedThis, savedArgs);
+                        savedArgs = savedThis = null;
+                    }
+                }, ms);
+            }
+            return wrapper;
+        }
+    );
+});
